6da5908575b7a0f3ee8937a11d469cec2bc1eda8
kotekaman <abdul.haris.djafar@gmail.com>
2020-04-10 13:32:24 +0700

    hasil yang baru

--- flight24/crawl.go
+++ flight24/crawl.go
@@ -3,6 +3,7 @@ package flight24
 import (
 	"CrawlerTable/lembarsaham"
 	"encoding/csv"
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"log"
 	"os"
@@ -15,17 +16,24 @@ func RemoveIndex(s []string, index int) []string {
 }
 
 func Crawler(filename string, delay time.Duration) {
-	var row []string
+	var row, rowair []string
 	var rows [][]string
 	var headings []string
 	dataAirlineCodee := []string{"n773ck", "EW-511TQ", "N773CK", "TC-LJS", "A7-BFT", "D-AALN"}
 	for _, data := range dataAirlineCodee {
+		rowair = nil
 		doc := lembarsaham.GetBody("https://www.flightradar24.com/data/aircraft/" + data)
+
+		doc.Find(".row.h-30.p-l-20.p-t-5").Each(func(index int, table *goquery.Selection) {
+			table.Find("span.details").Each(func(indexs int, data *goquery.Selection) {
+				space := regexp.MustCompile(`\s+`)
+				s := space.ReplaceAllString(data.Text(), " ")
+				rowair = append(rowair, s)
+			})
+		})
+		fmt.Println(len(rowair))
 		doc.Find("table").Each(func(index int, tablehtml *goquery.Selection) {
 			tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
-				rowhtml.Find("th").Each(func(indexth int, tableheading *goquery.Selection) {
-
-				})
 				rowhtml.Find("td").Each(func(indexth int, tablecell *goquery.Selection) {
 					space := regexp.MustCompile(`\s+`)
 					s := space.ReplaceAllString(tablecell.Text(), " ")
@@ -34,9 +42,13 @@ func Crawler(filename string, delay time.Duration) {
 				if len(row) > 3 {
 					row[0] = row[5]
 					RemoveIndex(row, 5)
-					RemoveIndex(row, 13)
-					RemoveIndex(row, 12)
-					RemoveIndex(row, 11)
+					row[11] = rowair[0]
+					row[12] = rowair[1]
+					row[13] = rowair[2]
+					row = append(row, rowair[3])
+					row = append(row, rowair[4])
+					row = append(row, rowair[5])
+					row = append(row, rowair[6])
 					rows = append(rows, row)
 				}
 				row = nil
@@ -58,7 +70,8 @@ func Crawler(filename string, delay time.Duration) {
 		}
 	}
 	defer file.Close()
-	headings = []string{"NO PENERBANGAN", "", "TANGGAL PENERBANGAN", "DARI", "MENUJU", "", "STD", "ATD", "STA", "", "STATUS KEDATANGAN", "", "", ""}
+	headings = []string{"NO PENERBANGAN", "", "TANGGAL PENERBANGAN", "DARI", "MENUJU", "", "STD", "ATD", "STA", "",
+		"STATUS KEDATANGAN", "AIRCRAFT", "AIRLINE", "OPERATOR", "TYPE CODE", "CODE1", "CODE2", "MODE S"}
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
